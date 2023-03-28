8de70f200036316b5027453807c010d043efcc38
noon <noonnono01@gmail.com>
2023-03-28 22:48:05 +0700

    film comment

--- frontend/happy_group/src/app/page.tsx
+++ frontend/happy_group/src/app/page.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 import Header from '@/components/header/header'
+import Filmcomment from '@/components/filmcomment/filmcomment'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className={styles.title}>
-      <Header/>
-      <p>hello world</p></div>
+    <div>
+      <Filmcomment/>
+      </div>
   )
 }
