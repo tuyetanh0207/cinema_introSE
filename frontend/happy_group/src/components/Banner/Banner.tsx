c6baada5efdbfc74d355d1e947a27a1d78a60f1b
20120397 <quangtung1112002@gmail.com>
2023-04-05 11:11:36 +0700

    using_for

--- frontend/happy_group/src/components/Banner/Banner.tsx
+++ frontend/happy_group/src/components/Banner/Banner.tsx
@@ -2,64 +2,51 @@ import styles from './Banner.module.css'
 import { pic1,pic2,pic3,pic4} from '@/assets/imgs'
 import Image from 'next/image'
 
-const  bannerPic= [
-    { src: pic1, alt:'' },
-    { src: pic2, alt:'' },
-    { src: pic3, alt:'' },
-    { src: pic4, alt:'' },
-    { src: pic1, alt:'' },
-    //... nếu có nhiều hơn 4 ảnh thì thêm vào đây
-  ];
   
 
 export default function Banner () {
+
+    const Bannerpic = [
+        pic1,
+        pic2,
+        pic3,
+        pic4,
+        pic1,
+        pic2,
+        pic3,
+        pic4,
+    ];
 
     return (
         <div className={styles.Banner}>
             <div className={styles.slides}>
 
             {/* btn*/}
-                <input id={styles.radio1} type={"radio"} name="radio-btn"/>
-                <input id={styles.radio2} type={"radio"} name="radio-btn"/>
-                <input id={styles.radio3} type={"radio"} name="radio-btn"/>
-                <input id={styles.radio4} type={"radio"} name="radio-btn"/>
+
+                {Bannerpic.map((_, index) => (<input id={styles[`radio${index+1}`]} type={"radio"} name="radio-btn" key={index}/>))}
 
             {/* btn*/}
 
             {/* <!--image--> */}
 
-                <div className={styles.slide} id={styles.first}>
-                    <Image src={pic1} alt={''}  />
-                </div>
-                <div className={styles.slide}>
-                    <Image src={pic2} alt={''}  />
-                </div>
-                <div className={styles.slide}>
-                    <Image src={pic3} alt={''}  />
-                </div>
-                <div className={styles.slide}>
-                    <Image src={pic4} alt={''}  />
-                </div>
-
+                {Bannerpic.map((image, index) => (<div className={styles.slide} id={index === 0 ? styles.first : ''} key={index}><Image src={image} alt={''}  /></div>))}
 
             {/* <!--image--> */}
 
             {/* auto */}
                 <div className={styles.au_nav}>
-                    <div className={styles.auto_btn1}></div>
-                    <div className={styles.auto_btn2}></div>
-                    <div className={styles.auto_btn3}></div>
-                    <div className={styles.auto_btn4}></div>
+
+                    {Bannerpic.map((_, index) => (<div className={styles[`auto_btn${index+1}`]} key={index}/>))}
+                    
                 </div> 
             {/* auto*/}
             </div>
 
             {/* manual */}
             <div className={styles.manual_nav}>
-                <label htmlFor={styles.radio1} className={styles.manual_btn}></label>
-                <label htmlFor={styles.radio2} className={styles.manual_btn}></label>
-                <label htmlFor={styles.radio3} className={styles.manual_btn}></label>
-                <label htmlFor={styles.radio4} className={styles.manual_btn}></label>
+
+                {Bannerpic.map((_, index) => (<label htmlFor={styles[`radio${index+1}`]} className={styles.manual_btn} key={index}></label>))}
+                
             </div>
             {/* manual */}
 
