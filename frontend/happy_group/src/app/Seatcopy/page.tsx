801d6249fbadae8f2d580ca7bf028d91a8bd19f0
Tuyet Anh <84496906+tuyetanh0207@users.noreply.github.com>
2023-05-17 19:05:03 +0700

    checkout branch

--- frontend/happy_group/src/app/Seatcopy/page.tsx
+++ frontend/happy_group/src/app/Seatcopy/page.tsx
@@ -1,150 +1,90 @@
 "use client"
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './Seat.module.css';
-import movieAPI from '@/app/api/movieAPI';
+import movieAPI from '@/app/api/movieAPI'
 
 export default function Seat() {
-  const [seats, setSeats] = useState<any[]>([]);
-  const [selectedSeats, setSelectedSeats] = useState<string[]>([]);
+	const [seats, setSeats] = useState<any[]>([]);
+  
+	useEffect(() => {
+	  const fetchSeats = async () => {
+		try {
+		  const response = await movieAPI.getIDSeat();
+		  const seatArr = response.data;
+		  console.log("res: ", seatArr);
+		  setSeats(seatArr);
+		} catch (error) {
+		  console.error('Error fetching seats:', error);
+		}
+	  };
+  
+	  fetchSeats();
+	}, []);
+  
+	const seatsByRow: { [row: string]: any[] } = {};
 
-  useEffect(() => {
-    const fetchSeats = async () => {
-      try {
-        const response = await movieAPI.getIDSeat();
-        const seatArr = response.data;
-        console.log("res: ", seatArr);
-        setSeats(seatArr);
-      } catch (error) {
-        console.error('Error fetching seats:', error);
-      }
-    };
+	seats.forEach((seat) => {
+		const { row } = seat;
+		if (!seatsByRow[row]) {
+		seatsByRow[row] = [];
+		}
+		seatsByRow[row].push(seat);
+	});
+	
 
-    fetchSeats();
-  }, []);
+    return (
+		<>
+		
+		
 
-  const seatsByRow: { [row: string]: any[] } = {};
+        <div className={styles.body}>
+            <div className = {styles.movie_container}>
+		
+			<ul className = {styles.showcase}>
+			<li>
+				<div className = {styles.seat}  ></div>
+				<small>Trống</small>
+			</li>
+			
+			<li>
+				<div className={`${ styles.seat} ${styles.selected}`}></div>
+				<small>Đang chọn</small>
+			</li>
+			<li>
+			<div className={`${ styles.seat} ${styles.occupied}`}></div>
+				<small>Đã chọn</small>
+			</li>    
+			</ul>
+		
+		<div className = {styles.container}>
+		  <div className = {styles.screen}>SCREEN</div>
+		  
+		{Object.keys(seatsByRow).map((row) => (
+			<div key={row}>
+				<div className = {styles.row}>
+					{seatsByRow[row].map((seat) => (
+						<div key={seat._id}>
+							{seat.type === 1 ?<div className = {styles.seat}></div> : null}
 
-  seats.forEach((seat) => {
-    const { row } = seat;
-    if (!seatsByRow[row]) {
-      seatsByRow[row] = [];
-    }
-    seatsByRow[row].push(seat);
-  });
 
-  const handleSeatClick = (seatId: string) => {
-    setSelectedSeats((prevSelectedSeats) => {
-      if (prevSelectedSeats.includes(seatId)) {
-        // Nếu seat đã được chọn, xóa khỏi danh sách selectedSeats
-        return prevSelectedSeats.filter((id) => id !== seatId);
-      } else {
-        // Nếu seat chưa được chọn, thêm vào danh sách selectedSeats
-        return [...prevSelectedSeats, seatId];
-      }
-    });
-  };
+							{seat.type === 2 ? <div className={`${ styles.doubleSeat} ${styles.occupied}`}></div> : null}
+							{seat.isAvailable ? null : <div className={`${ styles.seat} ${styles.occupied}`}></div>}
+						</div>
+					))}
+				</div>
+			</div>
+      	))}
+		  
+		  
+		  <p className = {styles.text}>
+			Giá tiền khi bạn đặt <span id="count">0</span> ghế là <span id="total">0</span> ngàn đồng.
+		  </p>
 
-  const handleNextClick = () => {
-    selectedSeats.forEach((selectedSeat) => {
-      const seatIndex = seats.findIndex((seat) => seat._id === selectedSeat);
-      if (seatIndex >= 0) {
-        seats[seatIndex].isAvailable = false;
-      }
-    });
-    setSelectedSeats([]);
-  };
+		  <p>Địa chỉ các ghế: <span id="address"></span></p>
 
-  return (
-    <>
-      <div className={styles.body}>
-        <div className={styles.movie_container}>
-
-		<ul className = {styles.showcase}>
-		  <li>
-			<div className = {styles.seat}  ></div>
-			<small>Trống</small>
-		  </li>
-        
-		  <li>
-			<div className={`${ styles.seat} ${styles.selected}`}></div>
-			<small>Đang chọn</small>
-		  </li>
-		  <li>
-          <div className={`${ styles.seat} ${styles.occupied}`}></div>
-			<small>Đã chọn</small>
-		  </li>    
-		</ul>
-
-          <div className={styles.container}>
-		  	<div className = {styles.screen}>SCREEN</div>
-
-            {Object.keys(seatsByRow).map((row) => (
-              <div key={row}>
-                <div className={styles.row}>
-                  {seatsByRow[row].map((seat, index) => (
-                    <div
-                      key={seat._id}
-                      className={`${
-                        seat.type === 1
-                          ? `${styles.seat} ${
-                              index === 1 ? styles['margin-right'] : ''
-                            } ${
-                              index === seatsByRow[row].length - 2
-                                ? styles['margin-left']
-                                : ''
-                            }`
-                          : ''
-                      } ${
-                        seat.type === 2
-                          ? `${styles.doubleSeat} ${
-                              index === 0 ? styles['margin-right'] : ''
-                            } ${
-                              index === seatsByRow[row].length - 1
-                                ? styles['margin-left']
-                                : ''
-                            }`
-                          : ''
-                      } ${seat.isAvailable ? '' : styles.occupied} ${
-                        selectedSeats.includes(seat._id) ? styles.selected : ''
-                      }`}
-                      onClick={() => handleSeatClick(seat._id)}
-                    ></div>
-                  ))}
-                </div>
-              </div>
-            ))}
-
-				<p className={styles.text}>
-              Số lượng ghế đơn là{' '}
-              <span id="count">
-                {seats.filter((seat) => seat.type === 1 && selectedSeats.includes(seat._id)).length}
-              </span>
-            </p>
-
-            <p className={styles.text}>
-              Số lượng ghế đôi là{' '}
-              <span id="countdouble">
-                {seats.filter((seat) => seat.type === 2 && selectedSeats.includes(seat._id)).length}
-              </span>
-            </p>
-            <p className={styles.text}>
-              Vị trí ghế là{' '}
-              <span id="address">
-                {selectedSeats.map((seatId) => {
-                  const seat = seats.find((seat) => seat._id === seatId);
-                  if (seat) {
-                    const seatNumber = parseInt(seat.seatnumber);
-                    return `${seat.row}${seat.seatNumber}`;
-                  }
-                  return '';
-                }).join(', ')}
-              </span>
-            </p>
-          </div>
-
-		  <button className = {styles.bt}> tiếp theo </button>
+		</div>
+	  </div>
         </div>
-      </div>
-    </>
-  );
+        </>
+    )
 }
