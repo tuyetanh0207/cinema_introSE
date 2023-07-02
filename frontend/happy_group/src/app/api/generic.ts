d6a7b0f768aeb1896c4e8234ec03fac83a3ba3c1
Tuyet Anh <84496906+tuyetanh0207@users.noreply.github.com>
2023-07-02 14:47:55 +0700

    change baseURL

--- frontend/happy_group/src/app/api/generic.ts
+++ frontend/happy_group/src/app/api/generic.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const baseURL ="https://movie-ticket-booking-3svg.onrender.com" ||"http://localhost:8000";
-export const baseURL_user = "https://movie-ticket-booking-3svg.onrender.com" || "http://localhost:8000";
+export const baseURL =process.env.NEXT_PUBLIC_SERVER_URL|| "https://movie-ticket-booking-3svg.onrender.com" ||"http://localhost:8000";
+export const baseURL_user = process.env.NEXT_PUBLIC_SERVER_URL|| "https://movie-ticket-booking-3svg.onrender.com" || "http://localhost:8000";
 //"https://5dlr4.wiremockapi.cloud" ||  "https://localhost:8000/v1"||
 
 export const apiV1 = `${baseURL}/v1`;
