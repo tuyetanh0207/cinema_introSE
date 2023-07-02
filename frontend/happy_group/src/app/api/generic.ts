7c8214a908d35397d5f6fc8f86acc361f8bb23fc
Tuyet Anh <84496906+tuyetanh0207@users.noreply.github.com>
2023-07-02 14:44:14 +0700

    change admin API

--- frontend/happy_group/src/app/api/generic.ts
+++ frontend/happy_group/src/app/api/generic.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const baseURL ="http://localhost:8000";
-export const baseURL_user = "http://localhost:8000";
+export const baseURL ="https://movie-ticket-booking-3svg.onrender.com" ||"http://localhost:8000";
+export const baseURL_user = "https://movie-ticket-booking-3svg.onrender.com" || "http://localhost:8000";
 //"https://5dlr4.wiremockapi.cloud" ||  "https://localhost:8000/v1"||
 
 export const apiV1 = `${baseURL}/v1`;
