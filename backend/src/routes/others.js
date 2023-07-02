b91958b6468e20f86f5091e0293b7d3b5f098986
Tuyet Anh <84496906+tuyetanh0207@users.noreply.github.com>
2023-07-02 17:40:58 +0700

    Delete backend directory

--- backend/src/routes/others.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const otherController = require('../controllers/otherController')
-
-const router = new express.Router();
-
-router.get('/slides', otherController.getSlides);
-router.get('/theatres', otherController.getTheatres);
-module.exports = router;
